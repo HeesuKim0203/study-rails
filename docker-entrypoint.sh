9852a629ce79a8e1d7a64231cfe50317473ecac3
HeesuKim0203 <heesukim998@gmail.com>
2024-05-30 20:11:55 +0900

    chore : Update docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,9 @@ bundle exec rails db:prepare
 # Run migrations
 bundle exec rails db:migrate
 
+# Run seed
+bundle exec rails db:seed
+
 # Index the data
 bundle exec rails runner "Bill.find_each(&:index_document)"
 
