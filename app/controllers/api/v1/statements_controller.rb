6c9502801c651cd961a8fc8b375899b93ccf23ab
HeesuKim0203 <heesukim998@gmail.com>
2024-06-03 16:30:15 +0900

    fix : syntax miss

--- app/controllers/api/v1/statements_controller.rb
+++ app/controllers/api/v1/statements_controller.rb
@@ -4,25 +4,25 @@ module Api
       protect_from_forgery with: :null_session
 
       # def index
-      #   @statement = Statement.all
-      #   render json: @statement
+      #   statement = Statement.all
+      #   render json: statement
       # end
 
       def update
-        @statement = Statement.find(params[:id])
-        if @statement.update(statement_params)
-          render json: @statement, status: :ok
+        statement = Statement.find(params[:id])
+        if statement.update(statement_params)
+          render json: statement, status: :ok
         else
-          render json: { errors: @statement.errors.full_messages }, status: :unprocessable_entity
+          render json: { errors: statement.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def destroy
-        @statement = Statement.find(params[:id])
-        if @statement.destroy
+        statement = Statement.find(params[:id])
+        if statement.destroy
           render json: { message: 'Statements deleted successfully' }, status: :ok
         else
-          render json: { errors: @statement.errors.full_messages }, status: :unprocessable_entity
+          render json: { errors: statement.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
